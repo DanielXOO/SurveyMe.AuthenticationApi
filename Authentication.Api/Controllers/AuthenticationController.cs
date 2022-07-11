b6348053a7249e106d1463db27ec8244938b9b0a
DanielXOO <dankulinkovich@gmail.com>
2022-07-11 09:13:26 +0300

    Update packages

--- Authentication.Api/Controllers/AuthenticationController.cs
+++ Authentication.Api/Controllers/AuthenticationController.cs
@@ -8,19 +8,33 @@ using SurveyMe.Error.Models.Response;
 
 namespace Authentication.Api.Controllers;
 
+/// <summary>
+/// Controller for user registration
+/// </summary>
 [ApiController]
 public class AuthenticationController : Controller
 {
     private readonly IAccountService _accountService;
+    
     private readonly IMapper _mapper;
 
+    
+    /// <summary>
+    /// Controllers constructor
+    /// </summary>
+    /// <param name="accountService">Account service instance</param>
+    /// <param name="mapper">Automapper instance</param>
     public AuthenticationController(IAccountService accountService, IMapper mapper)
     {
         _accountService = accountService;
         _mapper = mapper;
     }
 
-    
+    /// <summary>
+    /// Registration endpoint
+    /// </summary>
+    /// <param name="userModel">Create user model</param>
+    /// <exception cref="BadRequestException"></exception>
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BaseErrorResponse))]
     [HttpPost("/authentication/registration")]
