37965aa01ece5c59ab1f95caa2f075b700eea43e
DanielXOO <dankulinkovich@gmail.com>
2022-07-12 12:48:06 +0300

    Add client credentials authentication

--- Authentication.Api/Configurations/Configurations.cs
+++ Authentication.Api/Configurations/Configurations.cs
@@ -14,11 +14,12 @@ public static class Configurations
 
     public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
     {
-        new("SurveyMeApi",  new[] {
+        new("UserInfoScope",  new[] {
             JwtClaimTypes.Name, 
             JwtClaimTypes.Id,
             JwtClaimTypes.Role
-        })
+        }),
+        new("ApisScope")
     };
 
     public static IEnumerable<Client> Clients =>
@@ -32,11 +33,25 @@ public static class Configurations
                 {
                     IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.OfflineAccess,
-                    "SurveyMeApi"
+                    "UserInfoScope"
                 },
                 ClientSecrets =
                 {
                     new Secret("client_secret".Sha256())
+                },
+                AllowOfflineAccess = true
+            },
+            new()
+            {
+                ClientId = "api",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes =
+                {
+                    "ApisScope"
+                },
+                ClientSecrets =
+                {
+                    new Secret("api_secret".Sha256())
                 },
                 AllowOfflineAccess = true
             }
@@ -45,15 +60,62 @@ public static class Configurations
     public static IEnumerable<ApiResource> ApiResources =>
         new List<ApiResource>
         {
-            new("SurveyMeApi")
+            new("Survey.Api")
             {
                 ApiSecrets =
                 {
-                    new Secret("api_secret".Sha256())
+                    new Secret("survey_secret".Sha256())
                 },
                 Scopes =
                 {
-                    "SurveyMeApi"
+                    "UserInfoScope",
+                    "TestScope"
+                }
+            },
+            new("Answers.Api")
+            {
+                ApiSecrets =
+                {
+                    new Secret("answers_secret".Sha256())
+                },
+                Scopes =
+                {
+                    "UserInfoScope"
+                }
+            },
+            new("SurveyPersonOptions.Api")
+            {
+                ApiSecrets =
+                {
+                    new Secret("options_secret".Sha256())
+                },
+                Scopes =
+                {
+                    "UserInfoScope",
+                    "ApisScope"
+                }
+            },
+            new("Statistics.Api")
+            {
+                ApiSecrets =
+                {
+                    new Secret("statistics_secret".Sha256())
+                },
+                Scopes =
+                {
+                    "UserInfoScope"
+                }
+            },
+            new("Persons.Api")
+            {
+                ApiSecrets =
+                {
+                    new Secret("persons_secret".Sha256())
+                },
+                Scopes =
+                {
+                    "UserInfoScope",
+                    "ApisScope"
                 }
             }
         };
